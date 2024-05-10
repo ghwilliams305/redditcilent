472c5edd7cffd43e41372366cba2fd329eed71fe
George Williams <george@localhost.localdomain>
2024-05-10 10:00:14 -0700

    sets up article page

--- src/pages/article/Article.js
+++ src/pages/article/Article.js
@@ -1,3 +1,31 @@
+import Comment from "../../components/Comment";
+import styles from "../../resources/css/article.module.css";
+
 export default function Article() {
-    
+    return (
+        <>
+            <figure>
+                <h2>Article Title</h2>
+                <div>
+                    <img src="" alt="Article Cover Image" />
+                </div>
+                <figcaption>
+                    <p><strong>Posted By:</strong> Author</p>
+                    <p>Time</p>
+                    <p>Comment #</p>
+                </figcaption>
+            </figure>
+            <section>
+                <p></p>
+            </section>
+            <section>
+                <h2></h2>
+                <div>
+                    <Comment />
+                    <Comment />
+                    <Comment />
+                </div>
+            </section>
+        </>
+    )
 }
