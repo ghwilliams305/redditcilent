673f1ac8cf08cfab470ce424b9e32507b1ac0713
George Williams <george@localhost.localdomain>
2024-05-10 11:05:23 -0700

    sets up article page css

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,12 +1,14 @@
+import styles from '../resources/css/comment.module.css';
+
 function Comment() {
     return (
-        <>
+        <article className={styles.comment}>
             <div>
-                <h1>Commentor</h1>
+                <h2 className={styles.author}>Commentor</h2>
                 <p>Time</p>
             </div>
             <p>Comments</p>
-        </>
+        </article>
     );
 }
 
--- src/pages/article/Article.js
+++ src/pages/article/Article.js
@@ -4,28 +4,28 @@ import styles from "../../resources/css/article.module.css";
 export default function Article() {
     return (
         <>
-            <figure>
-                <h2>Article Title</h2>
+            <figure className={styles.card}>
                 <div>
                     <img src="" alt="Article Cover Image" />
                 </div>
+                <h2>Article Title</h2>
                 <figcaption>
                     <p><strong>Posted By:</strong> Author</p>
-                    <p>Time</p>
+                    <p className={styles.time}>Time</p>
                     <p>Comment #</p>
                 </figcaption>
             </figure>
-            <section>
-                <p></p>
-            </section>
-            <section>
-                <h2></h2>
-                <div>
-                    <Comment />
-                    <Comment />
-                    <Comment />
-                </div>
-            </section>
+            <div className={styles.main}>
+                <p>Article Content Article Content Article Content Article Content Article Content Article Content Article Content Article Content</p>
+                <section>
+                    <h2>Comments</h2>
+                    <div>
+                        <Comment />
+                        <Comment />
+                        <Comment />
+                    </div>
+                </section>
+            </div>
         </>
     )
 }
